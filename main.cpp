8a88f8fae379faa22bc30d93b7c54ae484a8dbce
Jnyfah <jnyfaah@gmail.com>
2020-10-15 22:06:33 +0100

    source location

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@ int main() {
     std::vector<Token>m_tok = Tokenize(input);
 
     std::cout<<input<<std::endl;
-    std::cout<<input.length()<<std::endl;
     for (auto x : m_tok) {
         std::cout << std::left << std::setw(10) << "[" + x.value + "]"<< std::setw(15) << toString(x.type)<< std::setw(10)<< x.location.getLine()<< x.location.getRow()<<std::endl;
-        //std::cout <<"["<<x.value<<"]"<< '\t'<<toString(x.type)<< '\t' << x.location.getLine()<< " , "<< x.location.getRow()<<std::endl;
+        
     }
 
 
